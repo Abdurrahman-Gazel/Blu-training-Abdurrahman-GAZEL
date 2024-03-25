8b304c00cb6647f85b463ffcec73f55826c32fc8
Abdurrahman GAZEL <abodee123.mimo@gmail.com>
2024-03-25 18:38:42 +0300

    3. Commit

--- projects/ui/src/lib/table/table.component.ts
+++ projects/ui/src/lib/table/table.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+export interface PeriodicElement {
+  id: number;
+  name: string;
+  lastname: string;
+  email: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { id: 1, name: 'john', lastname: 'Doe', email: 'john@example.com' },
+  { id: 2, name: 'Mary', lastname: 'Moe', email: 'Mary@example.com' },
+  { id: 3, name: 'July', lastname: 'Dooley', email: 'July@example.com' },
+  { id: 4, name: 'john', lastname: 'Doe', email: 'john@example.com' },
+  { id: 5, name: 'Mary', lastname: 'Moe', email: 'Mary@example.com' },
+  { id: 6, name: 'July', lastname: 'Dooley', email: 'July@example.com' },
+];
 
 @Component({
   selector: 'lib-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.css'
+  styleUrl: './table.component.css',
 })
 export class TableComponent {
-
+  displayedColumns: string[] = ['id', 'name', 'lastname', 'email'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { TableComponent } from '../../projects/ui/src/lib/table/table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatTableModule, TableComponent],
 })
-export class AppModule { }
+export class AppModule {}
